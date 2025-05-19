c10c8dc0c0387487d99c207545c50cace60bb8f7
Paul Brissaud <pbrissaud.pro@gmail.com>
2025-05-19 20:51:34 +0200

    fix(query): remove unused query
    Signed-off-by: Paul Brissaud <pbrissaud.pro@gmail.com>

--- lib/queries/profile.ts
+++ lib/queries/profile.ts
@@ -4,11 +4,6 @@ function getUserProfile(client: TypedSupabaseClient) {
   return client.from("profiles").select("*").throwOnError().single();
 }
 
-function createUserProfile(client: TypedSupabaseClient, { name, userId }: { name: string; userId: string }) {
-  return client.from("profiles").insert({ name, user_id: userId }).throwOnError().single();
-}
-
 export const profile = {
   get: getUserProfile,
-  create: createUserProfile,
 };
