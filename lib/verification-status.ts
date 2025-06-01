69188b5ceadda9b0bf1cf8aae0322b05be1524f3
Paul Brissaud <pbrissaud.pro@gmail.com>
2025-06-02 00:06:08 +0200

    fix: remove opennext and cloudfalre deployment
    Signed-off-by: Paul Brissaud <pbrissaud.pro@gmail.com>

--- lib/verification-status.ts
+++ lib/verification-status.ts
@@ -1,47 +1,43 @@
-export type UuidType = string
-export type BigIntType = BigInt
-export type DateType = Date
+export type UuidType = string;
+export type BigIntType = BigInt;
+export type DateType = Date;
 
-
- export interface DynamicValidationStatus {
-  resources? :  {
-target :  {
-apiVersion : string
-kind : string
-name : string
-}
-checkResults :  {
-kind : string
-status : string
-message? : string
-}[]
-}[]
-  lastChecked? :  {
-Time: DateType
-}
-  error? : string
-  allPassed : boolean
-}
-
- export interface StaticValidationStatus {
-  resources? :  {
-target :  {
-apiVersion : string
-kind : string
-name : string
-}
-ruleResults :  {
-rule : string
-status : string
-message? : string
-}[]
-}[]
-  lastChecked? :  {
-Time: DateType
-}
-  error? : string
-  allPassed : boolean
+export interface DynamicValidationStatus {
+  resources?: {
+    target: {
+      apiVersion: string;
+      kind: string;
+      name: string;
+    };
+    checkResults: {
+      kind: string;
+      status: string;
+      message?: string;
+    }[];
+  }[];
+  lastChecked?: {
+    Time: DateType;
+  };
+  error?: string;
+  allPassed: boolean;
 }
 
-
-
+export interface StaticValidationStatus {
+  resources?: {
+    target: {
+      apiVersion: string;
+      kind: string;
+      name: string;
+    };
+    ruleResults: {
+      rule: string;
+      status: string;
+      message?: string;
+    }[];
+  }[];
+  lastChecked?: {
+    Time: DateType;
+  };
+  error?: string;
+  allPassed: boolean;
+}
--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,4 @@
 import type { NextConfig } from "next";
-import { initOpenNextCloudflareForDev } from "@opennextjs/cloudflare";
 
 const nextConfig: NextConfig = {
   eslint: {
@@ -42,4 +41,3 @@ const nextConfig: NextConfig = {
 };
 
 export default nextConfig;
-initOpenNextCloudflareForDev();
