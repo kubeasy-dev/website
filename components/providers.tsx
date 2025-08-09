2bb9bb08546c5e2e3299fcf8359e3677f4c00425
Paul Brissaud <pbrissaud.pro@gmail.com>
2025-08-10 00:17:52 +0200

    fix(tracking): refactor PostHog initialization and user identification logic for improved clarity
    Signed-off-by: Paul Brissaud <pbrissaud.pro@gmail.com>

--- components/providers.tsx
+++ components/providers.tsx
@@ -1,38 +1,48 @@
 "use client";
 
-import { Suspense, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Provider as JotaiProvider } from "jotai";
 import posthog from "posthog-js";
-import { PostHogProvider as PHProvider, usePostHog } from "posthog-js/react";
-import { usePathname, useSearchParams } from "next/navigation";
+import { PostHogProvider as PHProvider } from "posthog-js/react";
 import { useUser } from "@/hooks/use-user";
-import { tr } from "date-fns/locale";
 
 function PostHogProvider({ children }: Readonly<{ children: React.ReactNode }>) {
   const { data: user } = useUser();
 
   useEffect(() => {
-    posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
-      api_host: "/ingest",
-      persistence: "localStorage+cookie",
-      ui_host: "https://eu.i.posthog.com",
-      capture_pageview: true,
-      capture_pageleave: true,
-      person_profiles: "always",
-      debug: process.env.NODE_ENV === "development",
-      disable_session_recording: process.env.NODE_ENV === "development",
-    });
+    if (typeof window !== "undefined") {
+      // eslint-disable-next-line
+      (window as any).__PH_INIT__ = true;
+      // eslint-disable-next-line
+      if (!(window as any).__PH_INIT__) {
+        // eslint-disable-next-line
+        (window as any).__PH_INIT__ = true;
+        posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
+          api_host: "/ingest",
+          persistence: "localStorage+cookie",
+          ui_host: "https://eu.i.posthog.com",
+          capture_pageview: true,
+          capture_pageleave: true,
+          person_profiles: "always",
+          debug: process.env.NODE_ENV === "development",
+          disable_session_recording: process.env.NODE_ENV === "development",
+        });
+      }
+    }
   }, []);
 
   useEffect(() => {
     if (user) {
-      posthog.identify(user.id, {
-        email: user.email,
-        name: user.user_metadata.full_name,
-      });
+      // Identify if the distinct ID has changed
+      if (posthog.get_distinct_id() !== user.id) {
+        posthog.identify(user.id, {
+          email: user.email,
+          name: user.user_metadata.full_name,
+        });
+      }
     }
   }, [user]);
 
