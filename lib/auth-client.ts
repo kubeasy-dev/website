399bd627c7b624accdd708a2452544a1472c3a09
Paul Brissaud <pbrissaud.pro@gmail.com>
2025-10-22 17:15:59 +0200

    fix: delete baseURL parameters in auth
    Signed-off-by: Paul Brissaud <pbrissaud.pro@gmail.com>

--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -2,8 +2,6 @@ import { adminClient, apiKeyClient } from "better-auth/client/plugins";
 import { createAuthClient } from "better-auth/react";
 
 export const authClient = createAuthClient({
-  /** The base URL of the server (optional if you're using the same domain) */
-  baseURL: "http://localhost:3000",
   plugins: [apiKeyClient(), adminClient()],
 });
 
