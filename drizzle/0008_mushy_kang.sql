12de39ac85057793cc2115630c863a700e58631b
Paul Brissaud <pbrissaud.pro@gmail.com>
2026-01-04 23:53:38 +0100

    add drizzle migration

--- drizzle/0008_mushy_kang.sql
+++ drizzle/0008_mushy_kang.sql
@@ -6,6 +6,12 @@ CREATE TABLE "challenge_type" (
 	"updated_at" timestamp DEFAULT now() NOT NULL
 );
 --> statement-breakpoint
+
+INSERT INTO challenge_type (slug, name, description, logo, updated_at) VALUES
+('fix', 'Fix', 'Debug and repair broken Kubernetes configurations to restore application functionality.', NULL, NOW()),
+('build', 'Build', 'Create Kubernetes configurations from scratch to deploy applications.', NULL, NOW()),
+('migrate', 'Migrate', 'Migrate applications to Kubernetes or between different configurations.', NULL, NOW());--> statement-breakpoint
+
 ALTER TABLE "challenge" ADD COLUMN "type" text DEFAULT 'fix' NOT NULL;--> statement-breakpoint
 ALTER TABLE "challenge" ADD CONSTRAINT "challenge_type_challenge_type_slug_fk" FOREIGN KEY ("type") REFERENCES "public"."challenge_type"("slug") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
 CREATE INDEX "challenge_type_idx" ON "challenge" USING btree ("type");
