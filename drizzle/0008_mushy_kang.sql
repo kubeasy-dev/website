9bab3a07488bdf0377ba61239a6784af2bef3027
Paul Brissaud <pbrissaud.pro@gmail.com>
2026-01-04 23:49:05 +0100

    add drizzle migration

--- drizzle/0008_mushy_kang.sql
+++ drizzle/0008_mushy_kang.sql
@@ -6,7 +6,6 @@ CREATE TABLE "challenge_type" (
 	"updated_at" timestamp DEFAULT now() NOT NULL
 );
 --> statement-breakpoint
-ALTER TABLE "challenge" ALTER COLUMN "type" SET DATA TYPE text;--> statement-breakpoint
-ALTER TABLE "challenge" ALTER COLUMN "type" SET DEFAULT 'fix';--> statement-breakpoint
+ALTER TABLE "challenge" ADD COLUMN "type" text DEFAULT 'fix' NOT NULL;--> statement-breakpoint
 ALTER TABLE "challenge" ADD CONSTRAINT "challenge_type_challenge_type_slug_fk" FOREIGN KEY ("type") REFERENCES "public"."challenge_type"("slug") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
-DROP TYPE "public"."challenge_type";
+CREATE INDEX "challenge_type_idx" ON "challenge" USING btree ("type");
--- drizzle/0008_orange_dragon_man.sql
+++
@@ -1,3 +0,0 @@
-CREATE TYPE "public"."challenge_type" AS ENUM('build', 'fix', 'migrate');--> statement-breakpoint
-ALTER TABLE "challenge" ADD COLUMN "type" "challenge_type" DEFAULT 'fix' NOT NULL;--> statement-breakpoint
-CREATE INDEX "challenge_type_idx" ON "challenge" USING btree ("type");
